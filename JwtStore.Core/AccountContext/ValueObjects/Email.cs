a0c4d4e926a5b799205d21eccad22bf89c55f6be
Matheus Wisch <wisch.pg@outlook.com>
2025-04-17 10:55:09 -0300

    :sparkles: Feat: Create string extension

---
+++ JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -0,0 +1,12 @@
+﻿using JwtStore.Core.SharedContext.Extensions;
+using JwtStore.Core.SharedContext.ValueObjects;
+
+namespace JwtStore.Core.AccountContext.ValueObjects;
+
+public class Email : ValueObject
+{
+    private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+    public string Addres { get; }
+    public string Hash => Addres.ToBase64();
+}
