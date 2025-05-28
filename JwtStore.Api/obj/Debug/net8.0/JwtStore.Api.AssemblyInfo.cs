0a879469de3b0e21a2e01855c2cdd4093840f108
Matheus Wisch <wisch.pg@outlook.com>
2025-05-28 20:11:43 -0300

    dev

--- JwtStore.Api/obj/Debug/net8.0/JwtStore.Api.AssemblyInfo.cs
+++ JwtStore.Api/obj/Debug/net8.0/JwtStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c4d4e926a5b799205d21eccad22bf89c55f6be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e6e7534c52fa15aee01cf2d4d651822691f9e95")]
 [assembly: System.Reflection.AssemblyProductAttribute("JwtStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JwtStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ JwtStore.Core/AccountContext/ValueObjects/Verification.cs
@@ -0,0 +1,10 @@
+﻿using JwtStore.Core.SharedContext.ValueObjects;
+
+namespace JwtStore.Core.AccountContext.ValueObjects;
+
+public class Verification : ValueObject
+{
+    public string Code { get; } = Guid.NewGuid().ToString("N")[..6].ToUpper();
+    public DateTime? ExpiresAt { get; private set; }
+    
+}
