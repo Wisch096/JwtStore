24f0f99be26287932698a83b4dd5b08bf8ebfbf7
Matheus Wisch <wisch.pg@outlook.com>
2025-05-28 20:17:11 -0300

    feat: create verification value object

--- JwtStore.Core/AccountContext/ValueObjects/Verification.cs
+++ JwtStore.Core/AccountContext/ValueObjects/Verification.cs
@@ -5,6 +5,22 @@ namespace JwtStore.Core.AccountContext.ValueObjects;
 public class Verification : ValueObject
 {
     public string Code { get; } = Guid.NewGuid().ToString("N")[..6].ToUpper();
-    public DateTime? ExpiresAt { get; private set; }
-    
+    public DateTime? ExpiresAt { get; private set; } = DateTime.UtcNow.AddMinutes(5);
+    public DateTime? VerifiedAt { get; private set; } = null;
+    public bool IsActive => VerifiedAt != null && ExpiresAt == null;
+
+    public void Verify(string code)
+    {
+        if(IsActive)
+           throw new Exception("This code is already verified");
+        
+        if(ExpiresAt < DateTime.UtcNow)
+            throw new Exception("This code has expired");
+        
+        if(!string.Equals(code.Trim(), Code.Trim(), StringComparison.CurrentCultureIgnoreCase))
+            throw new Exception("Invalid code");
+
+        ExpiresAt = null;
+        VerifiedAt = DateTime.UtcNow;
+    }
 }
