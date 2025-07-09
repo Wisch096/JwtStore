adf08bf030d0459827cd0f9e56604b2fc75dce8b
Matheus Wisch <wisch.pg@outlook.com>
2025-07-08 21:46:48 -0300

    feat: add user secrets

--- JwtStore.Api/Extensions/AccountContextExtension.cs
+++ JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -1,6 +1,38 @@
-﻿namespace JwtStore.Api.Extensions;
+﻿using JwtStore.Core.Contexts.AccountContext.UseCases.Create;
+using JwtStore.Infra.Data;
+using MediatR;
 
-public class AccountContext
+namespace JwtStore.Api.Extensions;
+
+public static class AccountContextExtension
 {
+    public static void AddAccountContext(this WebApplicationBuilder builder)
+    {
+        #region Create
+
+        builder.Services.AddTransient<
+            JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts.IRepository,
+            JwtStore.Infra.Contexts.AccountContext.UseCases.Create.Repository>();
+        
+        builder.Services.AddTransient<
+            JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts.IService,
+            JwtStore.Infra.Contexts.AccountContext.UseCases.Create.Service>();
+
+        #endregion
+    }
     
+    public static void MapAccountEndpoints(this WebApplication app)
+    {
+      #region Create
+
+      app.MapPost("api/v1/users", async (Request request, IRequestHandler<Request, Response> handler) =>
+      {
+          var result = await handler.Handle(request, CancellationToken.None);
+          return result.IsSuccess
+              ? Results.Created($"api/v1/users/{result.Data?.Id}", result)
+              : Results.Json(result, statusCode: result.Status);
+      });
+
+      #endregion
+    }
 }
--- JwtStore.Api/obj/Debug/net8.0/JwtStore.Api.AssemblyInfo.cs
+++ JwtStore.Api/obj/Debug/net8.0/JwtStore.Api.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("fc079359-23f8-493f-9520-7e52c5d9df29")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+416946dd40947e8b9c402e9309285f320ec6e974")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3be357d3fc0ce079433d4f3cafc94bdd9a1a186a")]
 [assembly: System.Reflection.AssemblyProductAttribute("JwtStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JwtStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
